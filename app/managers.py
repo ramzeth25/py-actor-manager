1f564a97b4436c54dfb5fce53968c3dff9e15f5d
ramzeth <ramzeth25@gmail.com>
2026-03-31 21:07:01 +0300

    Solution

--- app/managers.py
+++ app/managers.py
@@ -3,4 +3,51 @@ import sqlite3
 from app.models import Actor
 
 
-# add manager here
+class ActorManager:
+    def __init__(self, db_name: str, table_name: str):
+        self.db_name = db_name
+        self.table_name = table_name
+        self.conn = sqlite3.connect(self.db_name)
+
+    def all(self):
+        actors_cursor = self.conn.execute(f"SELECT * FROM {self.table_name}")
+        return [Actor(row[2], row[0], row[1]) for row in actors_cursor]
+
+    def create(self, first_name: str, last_name: str):
+        self.conn.execute(f"INSERT INTO {self.table_name} "
+                          f"(first_name, last_name) "
+                          f"VALUES (?, ?)", (first_name, last_name))
+        self.conn.commit()
+
+
+    def update(self,
+               pk: int,
+               new_first_name: str,
+               new_last_name: str
+               ):
+        self.conn.execute(f"UPDATE {self.table_name} "
+                          f"SET first_name = ?, "
+                          f"last_name = ? "
+                          f"WHERE id = ?",
+                          (new_first_name,
+                           new_last_name,
+                           pk)
+                          )
+        self.conn.commit()
+
+    def delete(self, pk: int):
+        self.conn.execute(f"DELETE FROM {self.table_name} "
+                          f"WHERE id = ?",
+                          (pk,)
+                          )
+        self.conn.commit()
+
+
+
+
+if __name__ == "__main__":
+    manager = ActorManager("movies.sqlite","actors")
+    manager.create("JOHN", "PITT")
+    print(manager.all())
+
+
